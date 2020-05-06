3a5589cfd10bff5a3d868f3ab6e2c3ca01a48265
diego <diegovieira@uel.br>
2020-05-05 23:11:45 -0300

    Updates

---
+++ precache-manifest.075f839ac16b80590ebdc2a607970f5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7e2a42014e187c87c979f03f001bd97",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "053f5990be79bdd069c2",
+    "url": "/monsters-rolodex/static/css/main.40607ecf.chunk.css"
+  },
+  {
+    "revision": "f7b84cd810271619eca9",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "053f5990be79bdd069c2",
+    "url": "/monsters-rolodex/static/js/main.d0c08aae.chunk.js"
+  },
+  {
+    "revision": "e3f9453037e5f28f8167",
+    "url": "/monsters-rolodex/static/js/runtime-main.7c0f1f0f.js"
+  }
+]);
